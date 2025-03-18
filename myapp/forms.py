2cf40683bfd63d7c0578754fecffd8edd2d234f6
D3ps3y <notes4u99@gmail.com>
2025-03-17 23:09:41 -0700

    Name fix, attempt #2

--- myapp/forms.py
+++ myapp/forms.py
@@ -36,7 +36,7 @@ class CustomRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["firstname", "lastname", "email", "password1", "password2"]
+        fields = ["first_name", "last_name", "email", "password1", "password2"]
 
     def save(self, commit=True):
         user = super().save(commit=False)
