65678e14b981384528b0592d168fcbfc23f1f9bb
D3ps3y <notes4u99@gmail.com>
2025-03-20 00:16:06 -0700

    Testing out an idea to have the home page, login page, and register page all in one. Attempt #7

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
-from .views import home, student_dashboard, faculty_dashboard, custom_login, custom_logout, register
+from .views import home, student_dashboard, faculty_dashboard, custom_logout
 
 urlpatterns = [
 
     path('', home, name='home'),
-    
-    path('login/', custom_login, name = 'login'),
-
     path('logout/', custom_logout, name = 'logout'),
-
-    path('register/', register, name='register'),
-
     path('student/', student_dashboard, name='student_dashboard'),
-
     path('faculty/', faculty_dashboard, name='faculty_dashboard'),
 
 ]
